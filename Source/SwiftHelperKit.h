dc3dcd5dc51a8d11f1b4af60cc178ec640939a39
Sebastian Volland <seb@baunz.net>
2016-03-26 15:38:37 +0100

    rename umbrella header

---
+++ Source/SwiftHelperKit.h
@@ -0,0 +1,10 @@
+//
+//  SwiftHelperKit.h
+//
+//  Copyright © 2015 Sebastian Volland. All rights reserved.
+//
+
+@import Foundation;
+
+FOUNDATION_EXPORT double SwiftHelperKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftHelperKitVersionString[];
